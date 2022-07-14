7ec04ea8a78a221ec5ed892eaff134bf53cfa9fe
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2022-07-14 14:03:05 +0200

    LT-4061: fix contract

--- client/Lykke.Service.CandlesHistory.Client/Models/PriceEvolution.cs
+++ client/Lykke.Service.CandlesHistory.Client/Models/PriceEvolution.cs
@@ -6,8 +6,8 @@ namespace Lykke.Service.CandlesHistory.Client.Models
     public class PriceEvolution
     {
         public PriceEvolutionPeriodType Period { get; set; }
-        public decimal? EodMidPrice { get; set; }
-        public decimal? LowMidPrice { get; set; }
-        public decimal? HighMidPrice { get; set; }
+        public decimal? FirstEod { get; set; }
+        public decimal? Highest { get; set; }
+        public decimal? Lowest { get; set; }
     }
 }
